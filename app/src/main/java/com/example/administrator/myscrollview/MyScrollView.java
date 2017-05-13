be399eff5aa3aefada53cf51f85e751c015444a2
386083520 <386083520@qq.com>
2017-05-13 11:01:13 +0800

    添加回弹效果

--- app/src/main/java/com/example/administrator/myscrollview/MyScrollView.java
+++ app/src/main/java/com/example/administrator/myscrollview/MyScrollView.java
@@ -13,12 +13,14 @@ import android.view.ViewGroup;
 
 public class MyScrollView extends ViewGroup{
     private GestureDetector detector;
+    private int currentIndex;
+    private  float startX ;
     public MyScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
         detector=new GestureDetector(context,new GestureDetector.SimpleOnGestureListener(){
             @Override
             public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                scrollBy((int)distanceX,(int)distanceY);
+                scrollBy((int)distanceX,0);
                 return true;
             }
         });
@@ -28,7 +30,41 @@ public class MyScrollView extends ViewGroup{
     public boolean onTouchEvent(MotionEvent event) {
         super.onTouchEvent(event);
         detector.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                System.out.println("onTouchEvent==ACTION_DOWN");
+                startX = event.getRawX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                System.out.println("onTouchEvent==ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                System.out.println("onTouchEvent==ACTION_UP");
+                float endX = event.getRawX();
+                float distanceX = endX - startX;
+                int tempIndex = currentIndex;
+                if (endX - startX > getWidth() / 2) {
+                    tempIndex--;
+                } else if (startX - endX > getWidth() / 2) {
+                    tempIndex++;
+                }
+
+                moveTo(tempIndex);
+
+                break;
+        }
         return true;
+    }
+
+    private void moveTo(int tempIndex) {
+        if(tempIndex < 0){
+            tempIndex = 0;
+        }
+        if(tempIndex >getChildCount()-1){
+            tempIndex = getChildCount()-1;
+        }
+        currentIndex = tempIndex;
+        scrollTo(currentIndex*getWidth(), 0);
     }
 
     @Override
